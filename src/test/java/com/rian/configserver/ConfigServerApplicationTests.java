bfaa272078eb4026e4aca0e76331211bc323da37
Fachrian LR <fachrianlr@gmail.com>
2024-07-15 13:13:41 +0700

    init commit

---
+++ src/test/java/com/rian/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rian.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
